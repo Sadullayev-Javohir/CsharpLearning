af10edde2d6b8e99415dba25ec9fbec5e5365e19
Sadullayev-Javohir <javohirsadullayev2024@gmail.com>
2025-04-15 16:16:30 +0500

    12_kun

---
+++ 12_kun/Program.cs
@@ -0,0 +1,27 @@
+﻿while (true)
+{
+  string savedUserName = "Javohir";
+  string savedPassword = "1234";
+  Console.WriteLine("***Login Tizimi***");
+  Console.WriteLine("Username kiriting: ");
+  string enterUsername = Convert.ToString(Console.ReadLine());
+  Console.WriteLine("Parolni kiriting: ");
+  string enterPassword = Convert.ToString(Console.ReadLine());
+  if (savedUserName == enterUsername && savedPassword == enterPassword)
+  {
+    Console.WriteLine("Tizimga Xush kelibsiz");
+    break;
+  }
+  else if (savedUserName != enterUsername && savedPassword == enterPassword)
+  {
+    Console.WriteLine("Username Xato");
+  }
+  else if (savedUserName == enterUsername && savedPassword != enterPassword)
+  {
+    Console.WriteLine("Password Xato");
+  }
+  else
+  {
+    Console.WriteLine("Username va Password Xato");
+  }
+}
