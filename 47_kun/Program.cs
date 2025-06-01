50b85f0b6f502123fe4d4dcaf7d3996dc4b7c7d7
Javohir <javohirsadullayev2024@gmail.com>
2025-06-01 22:56:07 +0500

    47_day

---
+++ 47_kun/Program.cs
@@ -0,0 +1,34 @@
+﻿// Juft va toq sonlarni ajratish: Massivdagi juft va toq sonlarni alohida massivlarga joylashtiring
+
+// int[] sonlar = new int[] {0, 1, 2, 3, 4, 5, 6, 7 };
+// List<int> juftlar = new List<int>() { };
+// List<int> toqlar = new List<int>() { };
+
+// for (var i = 0; i < sonlar.Length; i++)
+// {
+//   if (sonlar[i] != 0 && sonlar[i] % 2 == 0)
+//   {
+//     juftlar.Add(sonlar[i]);
+//   }
+//   if (sonlar[i] % 2 != 0)
+//   {
+//     toqlar.Add(sonlar[i]);
+//   }
+// }
+// Console.WriteLine("Toqlar: ");
+// foreach (var i in toqlar)
+// {
+//   Console.Write(i + " ");
+// }
+// Console.WriteLine();
+// Console.WriteLine("\nJuftlar: ");
+// foreach (var i in juftlar)
+// {
+//   Console.Write(i + " ");
+// }
+
+// Takrorlanuvchi elementlarni topish: Massivda necha marta takrorlanuvchi elementlarni toping
+
+int[] sonlar = { 1, 2, 2, 3, 3, 4, 5, 6, 6, 6 };
+bool[] tekshirilgan = new bool[sonlar.Length];
+Console.WriteLine("hey leave me alone");
