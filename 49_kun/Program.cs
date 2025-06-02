9eb88af18932e936916bb61e29352824f386bc80
Javohir <javohirsadullayev2024@gmail.com>
2025-06-03 01:22:21 +0500

    49_day

---
+++ 49_kun/Program.cs
@@ -0,0 +1,35 @@
+﻿// 1. ref bilan sonni ikki barobar oshirish:
+
+// void DoubleValue(ref int x) => x *= 2;
+
+// int num = 5;
+// DoubleValue(ref num);
+// Console.WriteLine(num);
+
+
+
+// 2. out yordamida kvadratni qaytarish:
+// void Square(int num, out int square) => square = num * num;
+
+// int num = 5;
+// int result;
+// Square(num, out result);
+// Console.WriteLine(result);
+
+
+// 3. params yordamida har xil sonlarni ko'paytirish:
+
+// int Multiply(params int[] numbers)
+// {
+//   int add = 1;
+//   foreach (var i in numbers)
+//   {
+//     add *= i;
+//   }
+//   return add;
+// }
+
+// int result = Multiply(2, 5, 7, 8);
+// Console.WriteLine(result);
+
+
