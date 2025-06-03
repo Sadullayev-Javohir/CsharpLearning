3951d5c8ccf50fc89d5bb10a007ccd7481b1f930
Javohir <javohirsadullayev2024@gmail.com>
2025-06-04 00:36:47 +0500

    50_kuny

---
+++ 50_kun/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+  static void Main()
+  {
+    // Console.WriteLine(Regex.IsMatch("abc123def", @"\d{3}"));
+    // Console.WriteLine(Regex.IsMatch("@#5@!()", @"\w*"));
+    // Console.WriteLine(Regex.IsMatch("ab_-a", @"^\W$"));
+    // Console.WriteLine(Regex.IsMatch("color", @"colou?r"));
+    // Console.WriteLine(Regex.IsMatch("colour", @"colou?r"));
+    // Console.WriteLine(Regex.IsMatch("gray", @"gr[ae]y"));
+    // Console.WriteLine(Regex.IsMatch("grey", @"gr[a?e]y"));
+    Console.WriteLine(Regex.IsMatch("#", @"[^abc]"));
+
+  }
+}
