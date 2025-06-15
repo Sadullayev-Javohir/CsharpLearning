5b90f1418576a1042460d40eb9f5ff766707c251
Javohir <javohirsadullayev2024@gmail.com>
2025-06-15 23:56:17 +0500

    57_kun

---
+++ 57_kun/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Car
+{
+  private int speed;
+  public int Speed
+  {
+    get { return speed; }
+    set { speed = value; }
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    Car car1 = new Car();
+    car1.Speed = 100;
+    Console.WriteLine($"{car1.Speed}");
+  }
+}
